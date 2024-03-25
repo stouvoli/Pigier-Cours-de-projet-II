14bf3952865e566ec52c9cfb0dc76e1673930771
King Ballo <stouvoli@gmail.com>
2024-03-25 10:07:34 +0000

    Create calculatrice.java

---
+++ chapitre 02/calculatrice.java
@@ -0,0 +1,24 @@
+public class Calculatrice {
+
+    public static void main(String[] args) {
+        // Déclaration des variables
+        int nombre1 = 10;
+        int nombre2 = 5;
+
+        // Addition
+        int somme = nombre1 + nombre2;
+        System.out.println("Somme : " + somme);
+
+        // Soustraction
+        int difference = nombre1 - nombre2;
+        System.out.println("Différence : " + difference);
+
+        // Multiplication
+        int produit = nombre1 * nombre2;
+        System.out.println("Produit : " + produit);
+
+        // Division
+        int quotient = nombre1 / nombre2;
+        System.out.println("Quotient : " + quotient);
+    }
+}
