0c616562f6d6d4e360060b9f033cb817af5e65d0
King Ballo <stouvoli@gmail.com>
2024-03-25 10:11:58 +0000

    Create hello-world-app.java

---
+++ chapitre 02/hello-world-app.java
@@ -0,0 +1,9 @@
+/**
+ * La classe HelloWorldApp implémente une application qui
+ * affiche simplement "Hello World!" sur la sortie standard.
+ */
+class HelloWorldApp {
+    public static void main(String[] args) {
+        System.out.println("Hello World!"); // Affiche la chaîne de caractères.
+    }
+}
