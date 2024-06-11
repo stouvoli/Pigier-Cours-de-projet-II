0f9fe5e8519b35e9540cd90c9d2cfd349b4b5f77
King Ballo <stouvoli@gmail.com>
2024-06-11 15:27:59 +0000

    Create BookShelf02.java

---
+++ chapitre08/BookShelf02.java
@@ -0,0 +1,19 @@
+package bookstoread;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Collections;
+import java.util.List;
+
+public class BookShelf {
+	private final List<String> books = new ArrayList<>();
+
+	public List<String> books() {
+		return Collections.emptyList();
+	}
+
+	public void add(String... booksToAdd) {
+		Arrays.stream(booksToAdd).forEach(book -> books.add(book));
+
+	}
+}
